931d5d6e2aa699830ccb6413003342c1fd80bd1d
Martin Heredia <Martin Heredia>
2021-11-11 15:37:35 -0300

    Subida testing ivore

---
+++ formulario.php
@@ -0,0 +1,20 @@
+<?php
+    if(isset($_POST['correo']) && isset($_POST['mensaje'])){
+        $correo = $_POST['correo'];
+        $mensaje = $_POST['mensaje'];
+
+        $from = $correo;
+        $to = "mhere92@gmail.com";//ivoreindumentaria@hotmail.com
+        $subject = "Nuevo mensaje de". $correo;
+        $message = $mensaje;
+        $headers = "From: ". $from;
+
+        if(mail($to,$subject,$message,$headers)) {
+            echo 1;
+        }else{
+            echo 0;
+        };
+    }else{
+        echo 0;
+    }
+?>
